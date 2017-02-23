ffacc2ad54ea032e41e2f6e4ef7b2609023c9aea
DESKTOP-KH82K9O\FranRoss <fran.ross.genius@gmail.com>
2017-02-23 21:28:23 +0100

    ranking finito

---
+++ HashCode2017/Managers/Ranking.cs
@@ -0,0 +1,55 @@
+﻿using HashCode2017.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2017.Managers
+{
+    public class Ranking
+    {
+        public static BufferModel CalculateRanking(
+            List<VideoRequestOnEndPoint> requests,
+            InputModel input)
+        {
+            BufferModel Result = new BufferModel();
+
+            CacheServer[] Caches = input.ChaceServers;
+            EndPoint[] EndPoints = input.EndPoints;
+
+            Parallel.ForEach(requests, (request) =>
+            {
+                EndPoint CurrentEndPoint = request.Endpoint;
+                foreach (Tuple<CacheServer, int> CacheServer in CurrentEndPoint.Caches)
+                {
+                    int Rank = 0;
+
+                    List<EndPoint> EndPointsForCurrentCache = 
+                        EndPoints.Where(e => e.Caches.Where(c => c.Item1.Id == CacheServer.Item1.Id).Count() > 0).ToList();
+                    EndPointsForCurrentCache = 
+                        EndPointsForCurrentCache.Where(e => input.GetVideoRequestsByEndpoint(e).Where(r => r.Video.Id == request.Video.Id).Count() > 0).ToList();
+
+                    foreach (EndPoint EndPointToRank in EndPointsForCurrentCache)
+                    {
+                        int LatencyToDataCenter = EndPointToRank.DatacenterLatency;
+                        Tuple<CacheServer, int> Couple =
+                            EndPointToRank.Caches.Where(e => e.Item1.Id == CacheServer.Item1.Id).FirstOrDefault();
+                        int LatencyToCurrentCacheServer = 0;
+                        if (Couple != null)
+                            LatencyToCurrentCacheServer = Couple.Item2;
+
+                        int PartialRank = LatencyToDataCenter - LatencyToCurrentCacheServer;
+
+                        if (PartialRank > 0)
+                            Rank = Rank + PartialRank;
+                    }
+
+                    Result.AddVideoServerRank(new VideoServerRankModel(request.Video.Id, CacheServer.Item1.Id, Rank));
+                }
+            });
+
+            return Result;
+        }
+    }
+}
